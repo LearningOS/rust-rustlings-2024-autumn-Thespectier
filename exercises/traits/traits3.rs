cdb2857769ea02e380f3e4795b53f48824192b92
Thespectier <2339523557@qq.com>
2024-10-11 15:46:48 +0800

    traits are down

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -8,10 +8,11 @@
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
 }
 
 struct SomeSoftware {
@@ -28,7 +29,7 @@ impl Licensed for OtherSoftware {} // Don't edit this line
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
     fn is_licensing_info_the_same() {
         let licensing_info = String::from("Some information");
